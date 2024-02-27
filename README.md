9d661b6acefcf8fdcfbe17572cfc06aa5bd37d85
lleonardx <lleonard.alves@gmail.com>
2024-02-27 14:29:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Automation Web - Chronos Academy
---
+++ src/test/java/br/com/chronosacademy/media/MediaTest.java
@@ -0,0 +1,24 @@
+package br.com.chronosacademy.media;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MediaTest {
+    //Aprovado
+    //Reprovado
+
+    @Test
+    public void validaAprovado(){
+        Media media = new Media();
+        String resultado = media.calculaMedia(5.0 , 5.0);
+        assertEquals("Aprovado", resultado);
+    }
+
+    @Test
+    public void validaReprovado(){
+        Media mediaReprova = new Media();
+        String resultadoReprova = mediaReprova.calculaMedia(4.9 , 5.0);
+        assertEquals("Reprovado", resultadoReprova);
+    }
+}
