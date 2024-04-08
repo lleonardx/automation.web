38a87e5e83a3646b3051f8fc2977540e8b237d79
lleonardx <lleonard.alves@gmail.com>
2024-04-08 14:53:56 -0400

    selenium - pages, estruturas e refactor

--- src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosacademy/automacaoWeb/TesteWeb.java
@@ -1,31 +1,32 @@
 package br.com.chronosacademy.automacaoWeb;
 
+import br.com.chronosacademy.core.Driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteWeb {
-    ChromeDriver driver;
+    Driver driverWeb;
+    WebDriver driver;
 
     @Before
     public void inicializaTeste(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driverWeb = new Driver("chrome");
+        driver = driverWeb.getDriver();
         driver.get("https://www.chronosacademy.com.br/");
 
     }
     @Test
     public void primeiroTest(){
-        String currentUrl = driver.getCurrentUrl();
-        String expected = "https://automationexercise.com/";
-        WebElement header = driver.findElement(By.xpath("/html/head/title"));
+        WebElement header = driver.findElement(By.xpath("/html/body/div/div/div/section[2]/div[3]/div/div/div[1]/div/h4"));
         String titulo = header.getText();
-        Assert.assertNotEquals("Automation Exercise", titulo);
+        Assert.assertEquals("Porque Tempo É Conhecimento.", titulo);
         System.out.println(titulo);
     }
 
--- src/test/java/br/com/chronosacademy/core/Driver.java
+++ src/test/java/br/com/chronosacademy/core/Driver.java
@@ -3,14 +3,33 @@ package br.com.chronosacademy.core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Driver {
 
     private WebDriver driver;
 
-    public Driver(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public Driver(String navegador){
+        switch (navegador){
+            case "Ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            case "Firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "Edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
         //driver.manage().window().maximize();
     }
     public WebDriver getDriver(){
